bfd3be9e23d876238f621b398c9972eff1f70371
Karantooo To <ctomasalvarezn@gmail.com>
2025-04-15 09:25:05 -0400

    feat: Se agrega dfs visual

--- juego/agente.py
+++ juego/agente.py
@@ -7,13 +7,15 @@ class Agente:
         self.movimientos_dados = 0
 
     def mover_horizontal(self, distancia):
-        print(str(self.x_frontera) + " " + str(self.columna + distancia))
-        if 0 <= self.fila + distancia <= self.x_frontera:
+        if 0 <= self.columna + distancia <= self.x_frontera:
             self.columna += distancia
             self.movimientos_dados += 1
+            return True
+        return False
 
     def mover_vertical(self, distancia):
-        print(str(self.y_frontera) + " " + str(self.fila + distancia))
         if 0 <= self.fila + distancia <= self.y_frontera:
             self.fila += distancia
             self.movimientos_dados += 1
+            return True
+        return False
--- juego/tablero.py
+++ juego/tablero.py
@@ -1,6 +1,7 @@
 import pygame
 from juego.constants import *
 from juego.agente import Agente
+import time
 
 class Casilla:
     def __init__(self, fila, columna, valor):
@@ -22,50 +23,24 @@ class Tablero:
         ]
         self.agente = Agente(*inicio,columnas - 1, filas - 1)
         self.tamano_celda = TAM_CASILLA
-        self.ancho = max(columnas * self.tamano_celda, self.tamano_celda * 3)
+        self.ancho = max(columnas * self.tamano_celda, self.tamano_celda * 4)
         self.alto = (filas + 1) * self.tamano_celda
         self.ventana = pygame.display.set_mode((self.ancho, self.alto))
+
+        self.coordenadas_vistas = set()
         pygame.display.set_caption("Laberinto Saltarín")
 
     def dibujar(self):
-        self.tablero_dibujar()
-        self.objetivo_dibujar()
-        self.agente_dibujar()
-        self.movimientos_cantidad_dibujar()
+        self.__tablero_dibujar()
+        self.__objetivo_dibujar()
+        self.__agente_dibujar()
+        self.__movimientos_cantidad_dibujar()
 
         pygame.display.flip()
 
-    def movimientos_cantidad_dibujar(self):
-        texto = pygame.font.SysFont(None, 24).render(
-            "Movimientos dados: " + str(self.agente.movimientos_dados),
-            True,
-            BLACK
-        )
-        self.ventana.blit(texto, (0, self.filas * self.tamano_celda))
-
-    def agente_dibujar(self):
-        ag_x = self.agente.columna * self.tamano_celda + self.tamano_celda // 2
-        ag_y = self.agente.fila * self.tamano_celda + self.tamano_celda // 2
-        pygame.draw.circle(self.ventana, BLUE, (ag_x, ag_y), self.tamano_celda // 3)
-
-    def objetivo_dibujar(self):
-        og_x = self.objetivo[1] * self.tamano_celda
-        og_y = self.objetivo[0] * self.tamano_celda
-        pygame.draw.rect(self.ventana, GREEN, (og_x, og_y, self.tamano_celda, self.tamano_celda))
-
-    def tablero_dibujar(self):
-        self.ventana.fill(WHITE)
-        for i in range(self.filas):
-            for j in range(self.columnas):
-                x = j * self.tamano_celda
-                y = i * self.tamano_celda
-                pygame.draw.rect(self.ventana, GRAY, (x, y, self.tamano_celda, self.tamano_celda), 1)
-                valor = self.casillas[i][j].valor
-                texto = pygame.font.SysFont(None, 24).render(str(valor), True, BLACK)
-                self.ventana.blit(texto, (x + 20, y + 20))
-
     def loop(self):
         corriendo = True
+        self.dfs_solucion(0.1)
         while corriendo:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
@@ -88,4 +63,66 @@ class Tablero:
 
             self.dibujar()
         pygame.quit()
+
+    def dfs_solucion(self, tiempo):
+
+        coordenadas = (self.agente.fila, self.agente.columna)
+
+        # Evitar visitar la misma casilla
+        if coordenadas in self.coordenadas_vistas:
+            return False
+
+        self.coordenadas_vistas.add(coordenadas)
+        self.dibujar()
+        time.sleep(tiempo)
+
+        desplazamiento = self.casillas[self.agente.fila][self.agente.columna].valor
+
+        # Intentar mover en las 4 direcciones
+        for mover in [self.agente.mover_horizontal, self.agente.mover_vertical]:
+            for signo in [1, -1]:
+                # Guardar coordenadas originales
+                fila_orig, col_orig = self.agente.fila, self.agente.columna
+
+                # Intentar mover
+                desplazamiento = signo * desplazamiento
+                mover(desplazamiento)
+                if self.dfs_solucion(tiempo):
+                    return True  # Terminar si se encontró la solución
+
+                # Volver atrás (backtrack)
+                self.agente.fila, self.agente.columna = fila_orig, col_orig
+                self.dibujar()
+                time.sleep(tiempo)
+
+        return False
+
+    def __movimientos_cantidad_dibujar(self):
+        texto = pygame.font.SysFont(None, 24).render(
+            "Movimientos dados: " + str(self.agente.movimientos_dados),
+            True,
+            BLACK
+        )
+        self.ventana.blit(texto, (0, self.filas * self.tamano_celda))
+
+    def __agente_dibujar(self):
+        ag_x = self.agente.columna * self.tamano_celda + self.tamano_celda // 2
+        ag_y = self.agente.fila * self.tamano_celda + self.tamano_celda // 2
+        pygame.draw.circle(self.ventana, BLUE, (ag_x, ag_y), self.tamano_celda // 3)
+
+    def __objetivo_dibujar(self):
+        og_x = self.objetivo[1] * self.tamano_celda
+        og_y = self.objetivo[0] * self.tamano_celda
+        pygame.draw.rect(self.ventana, GREEN, (og_x, og_y, self.tamano_celda, self.tamano_celda))
+
+    def __tablero_dibujar(self):
+        self.ventana.fill(WHITE)
+        for i in range(self.filas):
+            for j in range(self.columnas):
+                x = j * self.tamano_celda
+                y = i * self.tamano_celda
+                pygame.draw.rect(self.ventana, GRAY, (x, y, self.tamano_celda, self.tamano_celda), 1)
+                valor = self.casillas[i][j].valor
+                texto = pygame.font.SysFont(None, 24).render(str(valor), True, BLACK)
+                self.ventana.blit(texto, (x + 20, y + 20))
 
